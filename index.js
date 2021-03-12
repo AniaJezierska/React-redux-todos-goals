8aaba935c7bb7b2c90cf69df4d39bb80feeb58a4
Ania <candyrockstore5@gmail.com>
2021-03-12 11:55:49 +0100

    Updating the store object's methods

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
-function createStore() {
-    // The store should have four parts
-    // 1. The state
-    // 2. Get the state.
-    // 3. Listen to changes on the state.
-    // 4. Update the state
+// Library Code
+function createStore(reducer) {
+  // The store should have four parts
+  // 1. The state
+  // 2. Get the state.
+  // 3. Listen to changes on the state.
+  // 4. Update the state
 
-    let state
+  let state;
+  let listeners = [];
 
-    const getState = () => state
+  const getState = () => state;
 
-    return {
-        getState
-    }
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  };
+
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  };
 }
+
+//App Code
+function todos(state = [], action) {
+  if (action.type === "ADD_TODO") {
+    return state.concat([action.todo]);
+  }
+
+  return state;
+}
+
+const store = createStore(todos);
+
+store.subscribe(() => {
+  console.log("The new state is: ", store.getState());
+});
+
+store.dispatch({
+  type: "ADD_TODO",
+  todo: {
+    id: 0,
+    name: "Learn Redux",
+    complete: false,
+  },
+});
