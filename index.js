8ad5a9dcab27b69bc6e9a70700ba123fa0b0962d
Ania <candyrockstore5@gmail.com>
2021-03-29 21:35:00 +0200

    I implemented new actions

--- index.js
+++ index.js
@@ -30,13 +30,22 @@ function createStore(reducer) {
   };
 }
 
-//App Code
+// App Code
 function todos(state = [], action) {
-  if (action.type === "ADD_TODO") {
-    return state.concat([action.todo]);
+  switch (action.type) {
+    case "ADD_TODO":
+      return state.concat([action.todo]);
+    case "REMOVE_TODO":
+      return state.filter((todo) => todo.id !== action.id);
+    case "TOGGLE_TODO":
+      return state.map((todo) =>
+        todo.id !== action.id
+          ? todo
+          : Object.assign({}, todo, { complete: !todo.complete })
+      );
+    default:
+      return state;
   }
-
-  return state;
 }
 
 const store = createStore(todos);
