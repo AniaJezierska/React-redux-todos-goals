66459e9ab7e25bb58788d17deb159ddefb227208
Ania <candyrockstore5@gmail.com>
2021-04-01 13:06:45 +0200

    Added 'root redcer' for todos and goals'

--- index.js
+++ index.js
@@ -30,6 +30,16 @@ function createStore(reducer) {
   };
 }
 
+/*
+ * To remove a todo item, I called filter() on the state.
+ * This returns a new state (an array) with only todo items whose id's don't match the id of the todo I want to remove.
+ */
+
+/*
+ * To handle toggling a todo item, I want to change the value of the complete property on whatever id is passed along on the action.
+ * I mapped over the entire state, and if todo.id matched action.id, I used Object.assign() to return a new object with merged properties.
+ */
+
 // App Code
 function todos(state = [], action) {
   switch (action.type) {
@@ -48,7 +58,25 @@ function todos(state = [], action) {
   }
 }
 
-const store = createStore(todos);
+function goals(state = [], action) {
+  switch (action.type) {
+    case "ADD_GOAL":
+      return state.concat([action.goal]);
+    case "REMOVE_GOAL":
+      return state.filter((goal) => goal.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  };
+}
+
+const store = createStore(app);
 
 store.subscribe(() => {
   console.log("The new state is: ", store.getState());
@@ -58,7 +86,56 @@ store.dispatch({
   type: "ADD_TODO",
   todo: {
     id: 0,
-    name: "Learn Redux",
+    name: "Walk the dog",
     complete: false,
   },
+});
+
+store.dispatch({
+  type: "ADD_TODO",
+  todo: {
+    id: 1,
+    name: "Wash the car",
+    complete: false,
+  },
+});
+
+store.dispatch({
+  type: "ADD_TODO",
+  todo: {
+    id: 2,
+    name: "Go to the gym",
+    complete: true,
+  },
+});
+
+store.dispatch({
+  type: "REMOVE_TODO",
+  id: 1,
+});
+
+store.dispatch({
+  type: "TOGGLE_TODO",
+  id: 0,
+});
+
+store.dispatch({
+  type: "ADD_GOAL",
+  goal: {
+    id: 0,
+    name: "Learn Redux",
+  },
+});
+
+store.dispatch({
+  type: "ADD_GOAL",
+  goal: {
+    id: 1,
+    name: "Lose 20 pounds",
+  },
+});
+
+store.dispatch({
+  type: "REMOVE_GOAL",
+  id: 0,
 });
