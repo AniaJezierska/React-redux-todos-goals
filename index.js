c4264552d14d4f74600bfb1e61caf12d81547d5e
Ania <candyrockstore5@gmail.com>
2021-04-01 13:44:30 +0200

    Converted actions to use  constants instead of strings.

--- index.js
+++ index.js
@@ -41,13 +41,19 @@ function createStore(reducer) {
  */
 
 // App Code
+const ADD_TODO = "ADD_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const ADD_GOAL = "ADD_GOAL";
+const REMOVE_GOAL = "REMOVE_GOAL";
+
 function todos(state = [], action) {
   switch (action.type) {
-    case "ADD_TODO":
+    case ADD_TODO:
       return state.concat([action.todo]);
-    case "REMOVE_TODO":
+    case REMOVE_TODO:
       return state.filter((todo) => todo.id !== action.id);
-    case "TOGGLE_TODO":
+    case TOGGLE_TODO:
       return state.map((todo) =>
         todo.id !== action.id
           ? todo
@@ -60,9 +66,9 @@ function todos(state = [], action) {
 
 function goals(state = [], action) {
   switch (action.type) {
-    case "ADD_GOAL":
+    case ADD_GOAL:
       return state.concat([action.goal]);
-    case "REMOVE_GOAL":
+    case REMOVE_GOAL:
       return state.filter((goal) => goal.id !== action.id);
     default:
       return state;
@@ -83,7 +89,7 @@ store.subscribe(() => {
 });
 
 store.dispatch({
-  type: "ADD_TODO",
+  type: ADD_TODO,
   todo: {
     id: 0,
     name: "Walk the dog",
@@ -92,7 +98,7 @@ store.dispatch({
 });
 
 store.dispatch({
-  type: "ADD_TODO",
+  type: ADD_TODO,
   todo: {
     id: 1,
     name: "Wash the car",
@@ -101,7 +107,7 @@ store.dispatch({
 });
 
 store.dispatch({
-  type: "ADD_TODO",
+  type: ADD_TODO,
   todo: {
     id: 2,
     name: "Go to the gym",
@@ -110,17 +116,17 @@ store.dispatch({
 });
 
 store.dispatch({
-  type: "REMOVE_TODO",
+  type: REMOVE_TODO,
   id: 1,
 });
 
 store.dispatch({
-  type: "TOGGLE_TODO",
+  type: TOGGLE_TODO,
   id: 0,
 });
 
 store.dispatch({
-  type: "ADD_GOAL",
+  type: ADD_GOAL,
   goal: {
     id: 0,
     name: "Learn Redux",
@@ -128,7 +134,7 @@ store.dispatch({
 });
 
 store.dispatch({
-  type: "ADD_GOAL",
+  type: ADD_GOAL,
   goal: {
     id: 1,
     name: "Lose 20 pounds",
@@ -136,6 +142,6 @@ store.dispatch({
 });
 
 store.dispatch({
-  type: "REMOVE_GOAL",
+  type: REMOVE_GOAL,
   id: 0,
 });
